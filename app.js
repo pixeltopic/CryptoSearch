8998658aa17e110fbecf16d0ecad299c7b358135
pixeltopic <32804347+pixeltopic@users.noreply.github.com>
2018-01-22 17:23:17 -0800

    Data is now shown when searched
    Fixed some bugs when searching

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 /*
-TO DO:
-Will have a drop down menu to select currencies to calculate average
-Account for json response errors after fetching
-
-
 how app will work:
 user searches for a coin (.CoinName or .Symbol) and selects a currency to display details in. (USD, EUR, CAD)
 fetchDescription gets the Image and other basic data from the search input.
@@ -11,11 +6,14 @@ fetchAverage gets the statistics from the .Symbol and currency (USD, EUR, CAD)
 
 display all relevant information for the user.
 
+implement:
+when a user enters a new search, insert HTML for the search. User can click x to delete the search entry, but
+it requires a custom ID. If the search is invalid, show an error message.
 
+(currently, if search failed, there is no noticable message besides a console log, and
+Old data is still shown if your first search works, but next search fails. need fix)
 
-create area to display fetched average data
-
-Averages fetched from CoinBase. API provided by Cryptocompare. Add QuadrigaCX implementation and some other big exchanges
+Averages fetched 7 different exchanges. API provided by Cryptocompare.
 */
 
 
@@ -50,11 +48,12 @@ Averages fetched from CoinBase. API provided by Cryptocompare. Add QuadrigaCX im
                 }
             }
             
-            return -1;
+            return -1; // JSON fetched successfully, but Response was an error (maybe because search was invalid)
             
         }).then(function(obj) {
             if (obj !== -1) {
                 updatePicture(obj.BaseImageUrl, obj.ImageUrl);
+                updateSearchDetails(obj.CoinName, obj.Symbol, exchangeInput, curInput);
                 fetchAverage(obj.Symbol, exchangeInput, curInput);
             } else {
                 console.log("fetchDescription: Invalid CoinName or Symbol");
@@ -74,15 +73,34 @@ Averages fetched from CoinBase. API provided by Cryptocompare. Add QuadrigaCX im
         }).then(function(response) {
             if (response.Response === "Error") {
                 console.log("fetchAverage: API Url was Invalid, likely no exchange for that currency.");
+                return -1;
             } else {
                 console.log("Fetched Averages");
                 console.log(response);
+                
+                let statDetail = response.DISPLAY;
+                
+                return {
+                    PRICE: statDetail.PRICE,
+                    CHANGE24HOUR: statDetail.CHANGE24HOUR,
+                    CHANGEPCT24HOUR: statDetail.CHANGEPCT24HOUR,
+                    VOLUME24HOUR: statDetail.VOLUME24HOUR,
+                    VOLUME24HOURTO: statDetail.VOLUME24HOURTO,
+                    OPEN24HOUR: statDetail.OPEN24HOUR,
+                    LOW24HOUR: statDetail.LOW24HOUR,
+                    HIGH24HOUR: statDetail.HIGH24HOUR
+                }
             }
             
 //            console.log(response.Display);
+        }).then(function(obj) {
+            if (obj !== -1) {
+                updateStatsDetails(obj);
+            }
+            
         }).catch(function(err) {
             console.log(err);
-        });
+        });;
     }
 }
 
@@ -94,7 +112,9 @@ Averages fetched from CoinBase. API provided by Cryptocompare. Add QuadrigaCX im
         curInput: ".cur__input",
         btnInput: ".btn-fetch",
         
-        coinLogo: ".coinlogo"
+        coinLogo: ".coinlogo",
+        
+        statsDetail: ".stats__detail"
     }
     
     function getInput() {
@@ -121,11 +141,30 @@ Averages fetched from CoinBase. API provided by Cryptocompare. Add QuadrigaCX im
         });
     }
     
+    function updateSearchDetails(coin, coinSymbol, exchange, cur) {
+        document.getElementById("SEARCH-DETAILS").textContent = `Coin: ${coin} (${coinSymbol}) | Exchange: ${exchange} | Currency: ${cur}`;
+    }
+    
     function updatePicture(baseImageUrl, imageUrl) {
         document.querySelector(DOMstr.coinLogo).src = baseImageUrl + imageUrl;
     }
     
-    
+    function updateStatsDetails(obj) {
+        
+//        let details = document.querySelectorAll(DOMstr.statsDetail);
+//        let detailArr = Array.from(details);
+        
+        document.getElementById("PRICE").textContent = `Price Just Now: ${obj.PRICE}`;
+        document.getElementById("CHANGE24HOUR").textContent = `Change 24H: ${obj.CHANGE24HOUR} (${obj.CHANGEPCT24HOUR}%)`;
+        document.getElementById("VOLUME24HOUR").textContent = `Volume 24H: ${obj.VOLUME24HOUR} (${obj.VOLUME24HOURTO})`;
+        document.getElementById("OPEN24HOUR").textContent = `Open 24H: ${obj.OPEN24HOUR}`;
+        document.getElementById("LOWHIGH24HOUR").textContent = `Low/High 24H: ${obj.LOW24HOUR} - ${obj.HIGH24HOUR}`;
+        
+        let now = new Date();
+        let timestamp = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} - ${now.getDate()}-${now.getMonth()+1}-${now.getFullYear()}`
+        
+        document.getElementById("TIMESTAMP").textContent = `Timestamp: ${timestamp}`;
+    }
     
 }
 
